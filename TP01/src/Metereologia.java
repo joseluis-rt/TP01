231eea4acadbdf6ad9b381baed197a4c77febd7c
usuario <usuario@DESKTOP-SCS526T>
2021-02-14 20:14:42 -0300

    Commit TP01 novo1

--- TP01/src/Metereologia.java
+++ TP01/src/Metereologia.java
@@ -4,7 +4,7 @@ public class Metereologia {
 	static Scanner input = new Scanner(System.in);
 	
 	public static void main(String[] args) {
-		int opcoesMenu;
+		int opcoesMenu, mes, ano;
 		
 		do{
 	        System.out.print("_____________________________________\n");
@@ -32,34 +32,59 @@ public class Metereologia {
 
 	        switch(opcoesMenu){
 	        case 1 : //___________________________________________ Opcao 1 do Menu
-	        	System.out.print("______________________________\n");
+	        	System.out.print("__________________________________\n");
 	        	System.out.print("   (1) Entrada das temperaturas; \n");
-	        	System.out.print("______________________________\n");
+	        	System.out.print("__________________________________\n");
 	        	System.out.print("\n");
+	        	cadastrarTemperatura();
 	            break;
 	        case 2: //___________________________________________ Opcao 2 do Menu
-	        	System.out.print("_______________________________\n");
+	        	System.out.print("_________________________________________\n");
 	        	System.out.print("   (2) Calculo  da  temperatura  media; \n");
-	        	System.out.print("_______________________________\n");
+	        	System.out.print("_________________________________________\n");
 	        	System.out.print("\n");
+	        	
+	        	System.out.print("Digite o mes ( 1 - 12 ) : ");
+		    	mes = input.nextInt();
+		    	System.out.print("Insira o ano (2011-2020): ");
+		    	ano = input.nextInt();
+	        	temperaturaMedia(mes, ano);
 	            break;
 	        case 3 : //___________________________________________ Opcao 3 do Menu
-	        	System.out.print("____________________________\n");
+	        	System.out.print("__________________________________________\n");
 	        	System.out.print("   (3) Calculo  da  temperatura  minima; \n");
-	        	System.out.print("____________________________\n");
+	        	System.out.print("__________________________________________\n");
 	        	System.out.print("\n");
+	        	
+	        	System.out.print("Digite o mes ( 1 - 12 ) : ");
+		    	mes = input.nextInt();
+		    	System.out.print("Insira o ano (2011-2020): ");
+		    	ano = input.nextInt();
+	        	temperaturaMin(mes, ano);
 	            break;
 	        case 4 : //___________________________________________ Opcao 4 do Menu
-	        	System.out.print("___________________________________________\n");
+	        	System.out.print("__________________________________________\n");
 	        	System.out.print("   (4) Calculo  da  temperatura  maxima; \n");
-	        	System.out.print("___________________________________________\n");
+	        	System.out.print("__________________________________________\n");
 	        	System.out.print("\n");
+	        	
+	        	System.out.print("Digite o mes ( 1 - 12 ) : ");
+		    	mes = input.nextInt();
+		    	System.out.print("Insira o ano (2011-2020): ");
+		    	ano = input.nextInt();
+	        	temperaturaMax(mes, ano);
 	            break;
 	        case 5 : //___________________________________________ Opcao 5 do Menu
-	        	System.out.print("_______________________________\n");
+	        	System.out.print("_________________________________\n");
 	        	System.out.print("   (5) Relatorio meteorologico; \n");
-	        	System.out.print("_______________________________\n");
+	        	System.out.print("_________________________________\n");
 	        	System.out.print("\n");
+	        	
+	        	System.out.print("Digite o mes ( 1 - 12 ) : ");
+		    	mes = input.nextInt();
+		    	System.out.print("Insira o ano (2011-2020): ");
+		    	ano = input.nextInt();
+	        	relatorio(mes, ano);
 	            break;
 	        case 6 : //___________________________________________ Opcao 6 do Menu
 	        	System.out.print("_____________________________\n");
@@ -82,4 +107,27 @@ public class Metereologia {
 
 	}
 
+
+public static void cadastrarTemperatura() {                 //___________________________________________ Opcao 1 do Menu
+	
+	
+} 
+public static void temperaturaMedia(int mes, int ano) {     //___________________________________________ Opcao 2 do Menu
+	
+	
 }
+public static void temperaturaMin(int mes, int ano) {       //___________________________________________ Opcao 3 do Menu
+	
+	
+}
+public static void temperaturaMax(int mes, int ano) {       //___________________________________________ Opcao 4 do Menu
+	
+	
+}
+public static void relatorio(int mes, int ano) {            //___________________________________________ Opcao 5 do Menu
+	
+	
+}
+
+}
+
